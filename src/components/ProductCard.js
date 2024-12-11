95ece1a8c054d6fc6ab1ce6f58c3bb27fa4029e1
Nova3335 <134292081+Nova3335@users.noreply.github.com>
2024-12-11 18:18:32 +0200

    Update ProductCard.js

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -3,7 +3,7 @@ export const ProductCard = ({product}) => {
   const  { name,price,image} = product;
   return (
     <div className="productCard">
-      <img src={image}  alr={name} />
+      <img src={image}  alt={name} />
       <p className="name">{name}</p>
       <div className="action">
         <p>${price}</p>
